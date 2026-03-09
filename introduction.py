d1ed5f1199c8ab79c4dc6cdc8d896969a0012791
ASEMANI JOSHUA <145305697+AsemaniJoshua@users.noreply.github.com>
2026-03-09 18:26:22 +0000

    Add introduction script for basic numpy operations

---
+++ introduction.py
@@ -0,0 +1,17 @@
+# importing libraries
+import numpy as np
+
+# creating a python list
+my_list = [1, 2, 3, 4, 5]
+
+# converting the list to a numpy array
+my_array = np.array(my_list)
+
+# Multiplying the list by 2
+my_list = my_list * 2
+
+# Multiplying the array by 2
+my_array = my_array * 2
+
+# printing the array
+print(my_array)
